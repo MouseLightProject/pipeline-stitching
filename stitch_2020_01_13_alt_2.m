5e9ac8ff70ff82082049931761ef29c18a2d3a08
Adam L. Taylor <taylora@janelia.hhmi.org>
2020-04-17 14:55:49 -0400

    Driver scripts for 2020-01-13 sample

---
+++ stitch_2020_01_13_alt_2.m
@@ -0,0 +1,5 @@
+tile_folder_path = '/groups/mousebrainmicro/mousebrainmicro/data/2020-01-13/Tiling'  %#ok<NOPTS>
+pipeline_output_folder = '/nrs/mouselight/pipeline_output/2020-01-13'  %#ok<NOPTS>
+stitching_output_folder_path = '/groups/mousebrainmicro/mousebrainmicro/cluster/Reconstructions/2020-01-13/stitching-output-alt-2'  %#ok<NOPTS>
+
+stitch(tile_folder_path, pipeline_output_folder, stitching_output_folder_path) ;
