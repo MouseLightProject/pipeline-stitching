16145fa9759efc28e4c6b9a0a164677142bcbc09
Adam L. Taylor <taylora@janelia.hhmi.org>
2020-10-08 20:38:43 -0400

    Added driver script for 2020-09-15 sample

---
+++ stitch_2020_09_15.m
@@ -0,0 +1,4 @@
+tile_folder_path = '/groups/mousebrainmicro/mousebrainmicro/data/2020-09-15/Tiling'  %#ok<NOPTS>
+pipeline_output_folder = '/nrs/mouselight/pipeline_output/2020-09-15'  %#ok<NOPTS> 
+stitching_output_folder_path = '/groups/mousebrainmicro/mousebrainmicro/cluster/Reconstructions/2019-09-15/stitching-output'  %#ok<NOPTS>
+stitch(tile_folder_path, pipeline_output_folder, stitching_output_folder_path) ;
