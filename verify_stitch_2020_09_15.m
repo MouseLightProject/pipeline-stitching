1518427282060062aecf691d460e92b6415646b6
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-04-22 17:09:05 -0400

    Changes to some debugging code

--- verify_stitch_2020_09_15.m
+++ verify_stitch_2020_09_15.m
@@ -1,8 +1,9 @@
 % Specify the input/output folders
-sample_date = '2020-11-26'  %#ok<NOPTS>
+sample_date = '2020-09-15'  %#ok<NOPTS>
 tile_folder_path = sprintf('/groups/mousebrainmicro/mousebrainmicro/data/%s/Tiling', sample_date)  %#ok<NOPTS>
 pipeline_output_folder = sprintf('/nrs/mouselight/pipeline_output/%s', sample_date)  %#ok<NOPTS> 
-stitching_output_folder_path = sprintf('/groups/mousebrainmicro/mousebrainmicro/cluster/Reconstructions/%s/stitching-output-debug', sample_date)  %#ok<NOPTS>
+this_folder_path = fileparts(mfilename('fullpath')) ;
+stitching_output_folder_path = fullfile(this_folder_path, sprintf('%s-stitching-output-verify', sample_date))  %#ok<NOPTS>
 
 % Call the function that does the real work
 stitch(tile_folder_path, pipeline_output_folder, stitching_output_folder_path) ;
