a53dc4265fe7d07a604898e87dcc3bfd6994ebe3
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-05-06 17:46:26 -0400

    Switched to using tmt instead of mouselight_toolbox, tested still works fine

--- modpath.m
+++ modpath.m
@@ -3,5 +3,9 @@ function modpath()
     path_to_this_folder = fileparts(path_to_this_file) ;
     addpath(genpath(fullfile(path_to_this_folder, 'common'))) ;
     addpath(genpath(fullfile(path_to_this_folder, 'functions'))) ;
-    addpath(fullfile(path_to_this_folder, 'mouselight_toolbox')) ;
+    %addpath(fullfile(path_to_this_folder, 'mouselight_toolbox')) ;
+    
+    toolbox_path = fullfile(path_to_this_folder, 'tmt') ;
+    modpath_path = fullfile(toolbox_path, 'modpath.m') ;
+    run(modpath_path) ;    
 end
