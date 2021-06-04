f9b300d1a8264bcc03d398238b5bbc8ebd0b76ba
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-06-04 15:53:22 -0400

    Updated stitch_2021_05_22.m to explicitly specify new stitch() optional args

--- stitch_2021_05_22.m
+++ stitch_2021_05_22.m
@@ -6,9 +6,17 @@ modpath ;
 
 % Specify the input/output folders
 sample_date = '2021-05-22'  %#ok<NOPTS>
+do_force_computations = false
+do_perform_field_correction = true
+do_show_visualizations = false
 tile_folder_path = sprintf('/groups/mousebrainmicro/mousebrainmicro/data/acquisition/%s/', sample_date)  %#ok<NOPTS>
 pipeline_output_folder = sprintf('/nrs/mouselight/pipeline_output/%s', sample_date)  %#ok<NOPTS> 
 stitching_output_folder_path = sprintf('/groups/mousebrainmicro/mousebrainmicro/cluster/Reconstructions/%s/stitching-output', sample_date)  %#ok<NOPTS>
 
 % Call the function that does the real work
-stitch(tile_folder_path, pipeline_output_folder, stitching_output_folder_path) ;
+stitch(tile_folder_path, ...
+       pipeline_output_folder, ...
+       stitching_output_folder_path, ...
+       do_force_computations, ...
+       do_perform_field_correction, ...
+       do_show_visualizations) ;
